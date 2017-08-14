74c166bd8c469bbc0d91a8d75effed57094556c9
Julien Lemaître <julien@lemaitre-creation.fr>
2017-08-14 23:56:43 +0200

    Graph drawn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import BarChart from './components/bar-chart';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,9 @@ class App extends Component {
     this.state = {
       datas: {}
     };
+
+    this.getDatas = this.getDatas.bind(this);
+    this.sendNewDatas = this.sendNewDatas.bind(this);
   }
 
   componentDidMount() {
@@ -16,7 +20,21 @@ class App extends Component {
   }
 
   getDatas() {
+    const self = this;
+    axios.get('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+      .then(function (response) {
+        // console.log("response:", response);
+        self.sendNewDatas(response);
+      })
+      .catch(function (error) {
+        window.console.log("error:", error);
+      });
+  }
 
+  sendNewDatas(response) {
+    const datas = response.data;
+    console.log("response datas:", datas);
+    this.setState({ datas: datas });
   }
 
   render() {
@@ -25,7 +43,9 @@ class App extends Component {
         <div className="App-header">
           <h2>D3 Bar Chart</h2>
         </div>
-        <BarChart />
+        <BarChart
+          datas = {this.state.datas}
+        />
       </div>
     );
   }
--- src/components/bar-chart.js
+++ src/components/bar-chart.js
@@ -10,17 +10,61 @@ class BarChart extends Component {
     this.drawChart = this.drawChart.bind(this);
   }
 
-  componentDidUpdate() {
-    this.drawChart();
+  shouldComponentUpdate(nextProps) {
+    const useProps = nextProps.datas;
+    this.drawChart(useProps);
+
+    return false;
   }
 
-  drawChart() {
-    let svg = d3.select('svg');
+  drawChart(useProps) {
+    const datas = useProps.data;
+
+    const width = 800;
+    const height = 600;
+    const margin = {top: 20, bottom: 20, right: 20, left: 20};
+    const rectWidth = (width - margin.left - margin.right) / datas.length;
+
+    let svg = d3.select(this.svg)
+      .attr('width', width)
+      .attr('height', height);
+
+    let parseDatas = [];
+    datas.forEach(d => {
+      let date = d[0].split("-").join(" ");
+      date = new Date(date);
+      let gdp = d[1];
+      parseDatas.push({ date, gdp });
+    });
+
+    console.log("parseDatas:", parseDatas);
+
+    // Scales
+    const xExtent = d3.extent(parseDatas, d => d.date);
+    const xScale = d3.scaleTime()
+      .domain(xExtent)
+      .range([margin.left, width - margin.right]);
+
+    const yExtent = d3.extent(parseDatas, d => d.gdp);
+    const yScale = d3.scaleLinear()
+      .domain(yExtent)
+      .range([margin.bottom, height - margin.top]);
+
+    svg.selectAll('rect')
+      .data(parseDatas, d => d.date)
+      .enter().append('rect')
+      .attr('x', d => xScale(d.date))
+      .attr('y', d => height - margin.bottom - yScale(d.gdp))
+      .attr('width', rectWidth)
+      .attr('height', d => yScale(d.gdp))
+      .attr('fill', 'blue');
   }
 
   render() {
     return (
-      <svg></svg>
+      <svg
+        ref={(svg => this.svg = svg)}
+      ></svg>
     );
   }
 }
